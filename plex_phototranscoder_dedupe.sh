cd1c8835eacae6ba480c8ef51b81895dbe559728
Shawn Bruce <kantlivelong@gmail.com>
2018-07-10 20:23:32 -0400

    Add useful arguments. Shift things around.

--- plex_phototranscoder_dedupe.sh
+++ plex_phototranscoder_dedupe.sh
@@ -7,86 +7,117 @@
 # https://forums.plex.tv/t/plex-server-cache-over-310gb/274438/10
 #
 # Authors:
-# Shawn Bruce				 	- https://github.com/kantlivelong
-# SB Tech Services	(www.sbts.com.au)	- https://github.com/sbts
+# Shawn Bruce                   - https://github.com/kantlivelong
+# SB Tech Services  (www.sbts.com.au)   - https://github.com/sbts
 #
 #
 
-read -rst5 phototranscoder_path < <( readlink -f "$1"; )
 
+dry_run=0
+skip_warn=0
+use_relative=0
 cur_hash=""
 cur_file=""
 first_hash=""
 first_file=""
-
-
-if (( EUID != 0 )); then
-   echo "This script must be run as root!" 
-   exit 1
-fi
-
-
-if (( ${#} != 1 )); then
-    echo "Usage:"
-    echo "  ${0##*/} /path/to/PhotoTranscoder"
-    exit 1
-fi
-
-
-if [[ ! -d $phototranscoder_path ]]; then
-    echo "PhotoTranscoder path does not exist! - ${phototranscoder_path}"
-    exit 1
-fi
-
-
-echo "Please ensure Plex is stopped and you have a backup before continuing."
-echo "This process can take hours to complete."
-echo ""
-echo "Press ENTER when ready."
-read
 
 CleanupTempFiles() {
     rm $tmpfilelistp1
     rm $tmpfilelistp2
 }
 
-CreateTempfiles() {
-    trap CleanupTempFiles EXIT
-    tmpfilelistp1=$(mktemp)
-    tmpfilelistp2=$(mktemp)
+show_help() {
+    echo -e "Usage: ${0##*/} [OPTION]... /path/to/PhotoTranscoder"
+    echo -e "\t-h\t\t Help"
+    echo -e "\t-n\t\t Dry-run"
+    echo -e "\t-Y\t\t Skip warning message."
+    echo -e "\t-r\t\t Use relative symlinks."
 }
 
+if [ $# -lt 1 ]
+then
+    show_help
+    exit 1
+fi
 
-GenerateFileList() {
-    echo "Generating list of files (part 1)..."
-    while read -rst240 Ts Sum Name ; do
-        printf '%s %s %s\n' "$Sum" "$Ts" "$Name" >> $tmpfilelistp1
-    done < <( find "${phototranscoder_path}" -type f -printf "%C@\t" -exec md5sum "{}" \; )
+while getopts "hnYr" opt; do
+    case "$opt" in
+    h)
+        show_help
+        exit 0
+        ;;
+    n)
+        dry_run=1
+        ;;
+    Y)
+        skip_warn=1
+        ;;
+    r)
+        use_relative=1
+        ;;
+    esac
+done
 
-    echo "Generating list of files (part 2)..."
-    sort -o $tmpfilelistp2 $tmpfilelistp1
-}
 
-DeDuplicate() {
-    echo "Symlinking duplicates to originals..."
+read -rst5 phototranscoder_path < <( readlink -f "${@: -1}"; )
 
-    while read -rst5 cur_hash cur_ts cur_file; do
+if (( EUID != 0 )); then
+   echo "This script must be run as root!" 
+   exit 1
+fi
 
-        if [[ "${cur_hash}" == "${first_hash}" ]]; then
-            echo -e "     \tDUPE : ${cur_hash} - ${cur_file}"
-            ln -sf "${first_file}" "${cur_file}"
+if [[ ! -d $phototranscoder_path ]]; then
+    echo "PhotoTranscoder path does not exist! - ${phototranscoder_path}"
+    exit 1
+fi
+
+if [ $skip_warn -eq 0 ]
+then
+    echo "Please ensure Plex is stopped and you have a backup before continuing."
+    echo "This process can take hours to complete."
+    echo ""
+    echo "Press ENTER to continue."
+    read
+fi
+
+#CreateTempfiles
+trap CleanupTempFiles EXIT
+tmpfilelistp1=$(mktemp)
+tmpfilelistp2=$(mktemp)
+
+#GenerateFileList
+echo "Generating list of files (part 1)..."
+while read -rst240 Ts Sum Name ; do
+    printf '%s %s %s\n' "$Sum" "$Ts" "$Name" >> $tmpfilelistp1
+done < <( find "${phototranscoder_path}" -type f -printf "%C@\t" -exec md5sum "{}" \; )
+
+echo "Generating list of files (part 2)..."
+sort -o $tmpfilelistp2 $tmpfilelistp1
+
+#DeDuplicate
+echo "Symlinking duplicates to originals..."
+
+while read -rst5 cur_hash cur_ts cur_file
+do
+    if [[ "${cur_hash}" == "${first_hash}" ]]
+    then
+        echo -e "     \tDUPE : ${cur_hash} - ${cur_file}"
+        if [ $dry_run -eq 0 ]
+        then
+            if [ $use_relative -eq 1 ]
+            then
+                ln -sfr "${first_file}" "${cur_file}"
+            else
+                ln -sf "${first_file}" "${cur_file}"
+            fi
             chown --reference="${first_file}" "${cur_file}"
-        else
-            first_hash=$cur_hash
-            first_file=$cur_file
-            echo -e "=====\tORIG : ${first_hash} - ${first_file}"
         fi
+    else
+        first_hash=$cur_hash
+        first_file=$cur_file
+        echo -e "=====\tORIG : ${first_hash} - ${first_file}"
+    fi
 
-    done < $tmpfilelistp2
-}
-
-CreateTempfiles
-GenerateFileList
-DeDuplicate
+done < $tmpfilelistp2
 
 exit
